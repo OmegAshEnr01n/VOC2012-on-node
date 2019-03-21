e2cbb528b6dbe299b4d6f7fa8c5fea551170ff2c
OmegAshEnr01n <sobhit.me@gmail.com>
2019-03-21 16:20:20 +0800

    done V1.0.0

--- app/routes/vocapp.js
+++ app/routes/vocapp.js
@@ -59,7 +59,7 @@ router
             if(temp.fieldname != 'data'){
                 res.redirect('/');
             } else {
-                shell.exec('python '+path.join(__dirname,'..','py','test.py')+' '+ path.join(__dirname,'..','py','Model2.pt')+' '+path.join(__dirname,'..','public','files',temp.filename))
+                shell.exec('python '+path.join(__dirname,'..','py','uploadimg.py')+' '+ path.join(__dirname,'..','py','Model2.pt')+' '+path.join(__dirname,'..','public','files',temp.filename))
                 res.redirect('/vocapp/modelresult?file='+temp.filename);
             }
         }
@@ -85,7 +85,22 @@ router.get(
                     var c = e.split(' ');
                     snd[c[0]] = c[1];
                 });
-                socket.emit('getdata',snd);
+                var temp = {};
+                for(var k in snd){
+                    temp[snd[k]]=k;
+                }
+                delete temp[undefined];
+                var sorted = [];
+                for(var k in temp){
+                    sorted.push(k);
+                }
+                sorted.sort(function(a, b){return b-a});
+                var final = {};
+                for(var i =0; i<sorted.length; i++){
+                    var x = sorted[i];
+                    final[temp[x]] = x;
+                }
+                socket.emit('getdata',final);
             });
             
             
@@ -102,6 +117,69 @@ router.get(
     }
 );
 
+router.get(
+    '/browseimg',
+    checkWD,
+    function(req, res, next){
+        var class_names = [
+            'aeroplane',
+            'bicycle',
+            'bird',
+            'boat',
+            'bottle',
+            'bus',
+            'car',
+            'cat',
+            'chair',
+            'cow',
+            'diningtable',
+            'dog',
+            'horse',
+            'motorbike',
+            'person',
+            'pottedplant',
+            'sheep',
+            'sofa',
+            'train',
+            'tvmonitor'
+        ];
+        res.render('vocapp/browseclass.ejs',{
+            layout: 'vocapp.ejs',
+            classes: class_names
+        });
+    }
+);
+
+router.get(
+    '/browseimg/:name',
+    checkWD,
+    function(req, res){
+        var name = req.params.name;
+        console.log(name);
+        fs.readFile(path.join(__dirname,'..','py','browsedata',name),function(err, content){
+            if(err){
+                throw err;
+            }
+            data = JSON.parse(content);
+            imgs = []
+            var i;
+            data = JSON.parse(data);
+         
+            for(i = 0; i < 50; i++){
+                item = data[i];
+                var image = base64Img.base64Sync(item.img);
+                imgs.push(image);
+                
+            }
+            res.render('vocapp/browseimg.ejs',{
+                layout: 'vocapp.ejs',
+                imgs: imgs
+            });
+        });
+        
+    }
+);
+
 function checkWD(req, res, next){
     if(req.session.WD == null){
         res.redirect('/');
@@ -109,4 +187,5 @@ function checkWD(req, res, next){
         next();
     }
 }
+
 module.exports = router;
--- app/server.js
+++ app/server.js
@@ -21,6 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+app.use('/',express.static(path.join(__dirname,'assets')));
+app.use('/vocapp',express.static(path.join(__dirname,'assets')));
+// Spend a lot of time on this. So I think every miniapp need to provide the virtual links to assets
+
+
+
 
 app.use(session({
     resave: true,
@@ -34,22 +40,18 @@ app.use(flash());
 app.use('/', routes);
 app.use('/vocapp', vocapp);
 
-app.use('/',express.static(path.join(__dirname,'assets')));
-app.use('/vocapp',express.static(path.join(__dirname,'assets')));
-// Spend a lot of time on this. So I think every miniapp need to provide the virtual links to assets
-
-
-
 // Set Port
 app.set('port', (process.env.PORT || 3000));
 
 
 
-server =  app.listen(app.get('port'), function () {
+server = app.listen(app.get('port'), function () {
   console.log('Server started on port ' + app.get('port'));
 });
 
 const io = require('socket.io')(server);
 app.set('socketio',io);
+
+module.exports = app;
 
 
